7537ef2e46e55182a09a261b8493cb274ffa3fb5
Tin Willbeck <tin.willbeck@elev.ga.ntig.se>
2022-09-27 10:58:24 +0200

    1

---
+++ slagsmol/Program.cs
@@ -0,0 +1,41 @@
+﻿
+Console.WriteLine("Vad heter fighter A?");
+string a = Console.ReadLine();
+int hpa = 100;
+
+Console.WriteLine("Vad heter fighter B?");
+string b = Console.ReadLine();
+int hpb = 100;
+Console.ReadLine();
+Console.Clear();
+Random generator = new Random();
+int dmg = generator.Next(3, 17);
+
+while (hpa > 0 && hpb > 0){
+    
+    dmg = generator.Next(3, 17);
+    hpb = hpb - dmg;
+    Console.WriteLine($"{a} slog {b} och gjorde {dmg} skada, {b} har {hpb} HP!");
+
+    dmg = generator.Next(3, 17);
+    hpa = hpa - dmg;
+    Console.WriteLine($"{b} slog {a} och gjorde {dmg} skada, {a} har {hpa} HP!");
+
+    Console.WriteLine("Tryck på enter för att gå vidare");
+    Console.ReadLine();
+    Console.Clear();
+}
+
+if (hpa <= 0){
+    Console.WriteLine($"{b} Vann!");
+}
+else if (hpa <= 0 && hpb <= 0){
+    Console.WriteLine("Oavgort!");
+}
+else
+{
+    Console.WriteLine($"{a} Vann!");    
+}
+
+
+Console.ReadLine();
